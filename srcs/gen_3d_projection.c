802fde4cf301d6eb5f95e90facd788363bcb43fa
Lena MELARD <lmelard@e3r7p17.clusters.42paris.fr>
2022-12-14 18:43:04 +0100

    fixing merge issue

--- srcs/gen_3d_projection.c
+++ srcs/gen_3d_projection.c
@@ -6,7 +6,7 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 15:42:35 by cgaillag          #+#    #+#             */
-/*   Updated: 2022/12/14 18:38:29 by lmelard          ###   ########.fr       */
+/*   Updated: 2022/12/14 18:41:06 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,39 +123,39 @@ void	ft_generate_3d_projection(t_data *data)
 // 	}
 // }
 
-void	ft_generate_3d_projection(t_data *data)
-{
-	int				i;
-	t_proj			proj;
-	t_point			wall_top_pixel;
-	t_point			wall_bottom_pixel;
+// void	ft_generate_3d_projection(t_data *data)
+// {
+// 	int				i;
+// 	t_proj			proj;
+// 	t_point			wall_top_pixel;
+// 	t_point			wall_bottom_pixel;
 
-	ft_render_color_ceiling(data);
-	ft_render_color_floor(data);
-	ft_init_proj(&proj);
-	i = 0;
-	while (i < NUM_RAYS)
-	{
-		// pour supprimer l'effet fishball
-		proj.perp_distance = data->rays[i].distance * cos(data->rays[i].ray_angle - data->player.rotation_angle);
-		//proj.distance_proj_plane = (WIN_WIDTH / 2) / tan(FOV_ANGLE / 2);
-		proj.projected_wall_height = (TILE_SIZE / proj.perp_distance) * data->dist_proj_plane;
-		proj.wall_strip_height = (int) proj.projected_wall_height;
-		wall_top_pixel.y = (WIN_HEIGHT / 2) - (proj.wall_strip_height / 2);
-		if (wall_top_pixel.y < 0)
-			wall_top_pixel.y = 0;
-		wall_top_pixel.x = i;
-		wall_bottom_pixel.y = (WIN_HEIGHT / 2) + (proj.wall_strip_height / 2);
-		if (wall_bottom_pixel.y > WIN_HEIGHT)
-			wall_bottom_pixel.y = WIN_HEIGHT;
-		wall_bottom_pixel.x = i;
-		if (data->rays[i].wall_hit_vertical == TRUE)
-			proj.color = COLOR_LIGHT_PURPLE;
-		else
-			proj.color = COLOR_PURPLE;
-		// changer la couleur avec les éléments dans base
-		ft_draw_line2(data->cub, wall_top_pixel, wall_bottom_pixel, proj.color);
-		i++;
-	}
-}
+// 	ft_render_color_ceiling(data);
+// 	ft_render_color_floor(data);
+// 	ft_init_proj(&proj);
+// 	i = 0;
+// 	while (i < NUM_RAYS)
+// 	{
+// 		// pour supprimer l'effet fishball
+// 		proj.perp_distance = data->rays[i].distance * cos(data->rays[i].ray_angle - data->player.rotation_angle);
+// 		//proj.distance_proj_plane = (WIN_WIDTH / 2) / tan(FOV_ANGLE / 2);
+// 		proj.projected_wall_height = (TILE_SIZE / proj.perp_distance) * data->dist_proj_plane;
+// 		proj.wall_strip_height = (int) proj.projected_wall_height;
+// 		wall_top_pixel.y = (WIN_HEIGHT / 2) - (proj.wall_strip_height / 2);
+// 		if (wall_top_pixel.y < 0)
+// 			wall_top_pixel.y = 0;
+// 		wall_top_pixel.x = i;
+// 		wall_bottom_pixel.y = (WIN_HEIGHT / 2) + (proj.wall_strip_height / 2);
+// 		if (wall_bottom_pixel.y > WIN_HEIGHT)
+// 			wall_bottom_pixel.y = WIN_HEIGHT;
+// 		wall_bottom_pixel.x = i;
+// 		if (data->rays[i].wall_hit_vertical == TRUE)
+// 			proj.color = COLOR_LIGHT_PURPLE;
+// 		else
+// 			proj.color = COLOR_PURPLE;
+// 		// changer la couleur avec les éléments dans base
+// 		ft_draw_line2(data->cub, wall_top_pixel, wall_bottom_pixel, proj.color);
+// 		i++;
+// 	}
+// }
 
